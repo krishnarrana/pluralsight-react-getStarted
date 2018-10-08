f816661b94d334774ffa40f7c1f33afe2eb59b7a
unknown <krishna2121.rana@gmail.com>
2018-10-08 16:19:07 +0200

    Number selection added

--- 04-building-the-game-interface/play-nine/src/index.js
+++ 04-building-the-game-interface/play-nine/src/index.js
@@ -8,10 +8,9 @@ import './index.scss'
 class Stars extends Component{
     
     render(){
-        const numberOfStars= 1+ Math.floor(Math.random()*9);
         let stars= [];
-        for(let i=0; i<numberOfStars; i++){
-            stars.push(<FontAwesomeIcon className='icon' icon={faStar} />)
+        for(let i=0; i<this.props.randomNumbers; i++){
+            stars.push(<FontAwesomeIcon className='icon' icon={faStar} key={i} />)
         }
         return(
            <div className="col-sm-5">
@@ -25,7 +24,7 @@ class Buttons extends Component{
     render(){
         return(
            <div className="col-sm-2 text-center">
-                 <button className='btn btn-primary'>=</button>
+                 <button className='btn btn-primary' disabled={this.props.selectedNumbers.length===0}>=</button>
            </div>
         )
     }
@@ -36,20 +35,34 @@ class Answers extends Component{
         return(
            <div className="col-sm-5">
                  <div className="number-list">
-                    <span >1</span>
+                    {
+                        this.props.selectedNumbers.map((number,i)=>{
+                           return <span key={i}
+                           onClick={()=>this.props.unselectNumber(number)}
+                           >{number}</span>
+                        })
+                    
+                    }
                  </div>
            </div>
         )
     }
 }
 class Numbers extends Component{
+    numberClassName =(number)=>{
+        if(this.props.selectedNumbers.indexOf(number)>=0){
+            return "selected";
+        }
+    }
     render(){
         let numbers= [1,2,3,4,5,6,7,8,9]
         return(
            <div className="card text-center">
                 <div className="number-list">
                     {numbers.map((number,i)=>{
-                       return <span key={i}>{number}</span>
+                       return <span key={i} className={this.numberClassName(number)}
+                       onClick={()=>this.props.selectNumber(number)}
+                       >{number}</span>
                     })}
                 </div>
            </div>
@@ -57,16 +70,33 @@ class Numbers extends Component{
     }
 }
 class Game extends Component {
+    state={
+        selectedNumbers: [],
+        randomNumbers:1+ Math.floor(Math.random()*9)
+    }
+    selectNumber= (clickedNumber)=>{
+        if(this.state.selectedNumbers.indexOf(clickedNumber)>=0){
+            return;
+        }
+        this.setState(prevState=>({
+            selectedNumbers:prevState.selectedNumbers.concat(clickedNumber)
+        }));
+    }
+    unselectNumber= (clickedNumber)=>{
+        this.setState(prevState=>({
+            selectedNumbers:prevState.selectedNumbers.filter(number=> number !== clickedNumber)
+        }));
+    }
     render() { 
         return ( 
             <div className="container">
                 <h1>Game Nine</h1>
                 <div className="row" style={{marginBottom:'20px'}}>
-                    <Stars/>
-                    <Buttons/>
-                    <Answers/>
+                    <Stars randomNumbers={this.state.randomNumbers}/>
+                    <Buttons selectedNumbers={this.state.selectedNumbers} />
+                    <Answers selectedNumbers={this.state.selectedNumbers} unselectNumber= {this.unselectNumber}/>
                 </div>
-                <Numbers />
+                <Numbers  selectedNumbers={this.state.selectedNumbers} selectNumber={this.selectNumber}/>
             </div>
         );
     }
